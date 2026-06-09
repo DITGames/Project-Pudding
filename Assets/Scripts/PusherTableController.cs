e0c388048763078525a20f4f235c68d42a681d9b
Masatoshi Kitada (DITGames) <ditgames.inc@gmail.com>
2026-06-09 17:22:18 +0900

    feat: テーブルの動きを実装

---
+++ Assets/Scripts/PusherTableController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PusherTableController : MonoBehaviour
+{
+    [SerializeField]
+    float moveValue = 0.0f;
+
+    [SerializeField]
+    float intensity = 1.0f;
+
+    [SerializeField]
+    float speed = 1.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveValue = Mathf.Sin(Time.time * speed) * intensity;
+        transform.position = new Vector3(0.0f,0.0f, moveValue + (intensity));
+    }
+}
