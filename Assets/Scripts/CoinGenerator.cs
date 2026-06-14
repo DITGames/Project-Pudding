bee50c8993c07262149c5d71ff7ea42985a0b9d5
Masatoshi Kitada (DITGames) <ditgames.inc@gmail.com>
2026-06-14 23:32:43 +0900

    feat: コインのモデル入れ替えと物理挙動の調整

--- Assets/Scripts/CoinGenerator.cs
+++ Assets/Scripts/CoinGenerator.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class CoinGenerator : MonoBehaviour
@@ -12,6 +13,13 @@ public class CoinGenerator : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        StartCoroutine(generateCoin());
+    }
+
+    IEnumerator generateCoin()
+    {
+        yield return new WaitForSeconds(3);
+
         for (int i = 0; i < genarateNum; i++)
         {
             Vector3 pos = new Vector3(
@@ -19,6 +27,8 @@ public class CoinGenerator : MonoBehaviour
                 Random.Range(-transform.localScale.y * 0.5f, transform.localScale.y * 0.5f),
                 Random.Range(-transform.localScale.z * 0.5f, transform.localScale.z * 0.5f)
                 );
+
+            pos = pos + transform.position;
 
             Quaternion rot = Quaternion.Euler(
                 Random.Range(0f, 360f),
--- Assets/Scripts/PusherTableController.cs
+++ Assets/Scripts/PusherTableController.cs
@@ -11,16 +11,25 @@ public class PusherTableController : MonoBehaviour
     [SerializeField]
     float speed = 1.0f;
 
+    Rigidbody rb;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        moveValue = Mathf.Sin(Time.time * speed) * intensity;
-        transform.position = new Vector3(0.0f,0.0f, moveValue + (intensity));
+        float moveValue = Mathf.Sin(Time.time * speed) * intensity;
+
+        Vector3 targetPos = new Vector3(
+            0.0f,
+            0.0f,
+            moveValue + intensity
+        );
+
+        rb.MovePosition(targetPos);
     }
 }
